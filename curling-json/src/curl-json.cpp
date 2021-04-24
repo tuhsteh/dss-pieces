03d5ed30fffc80000a108659eb4b375348fc3bd9
tstear-on-ladder <t.stear@gmail.com>
2021-04-24 17:34:14 -0600

    A working sample from someone else, using winsock

--- curling-json/src/curl-json.cpp
+++ curling-json/src/curl-json.cpp
@@ -1,57 +1,154 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_net.h>
-#include <time.h>
+#include <stdio.h>
+#include <winsock.h>
 
 #include <cstdio>
 #include <iostream>
-#include <random>
 #include <string>
 
-int notMain(int argc, char* argv[]) {
+#define PORT 80
+#define USER_AGENT "winsock"
+#define HOSTNAME "date.jsontest.com"
+
+char *build_command_query(char *, char *);
+
+int main(int argc, char *argv[]) {
+
   using namespace std;
-  freopen("output.txt", "w", stdout);
-  freopen("error.txt", "w", stderr);
+  freopen("output.txt","w",stdout);
+  freopen("error.txt","w",stderr);
 
-  std::cout << "Starting up..." << std::endl;
-  std::cerr << "Starting up..." << std::endl;
+  int client_socket;
+  char *host;
+  struct hostent *hent;
+  int iplen = 15;  // XXX.XXX.XXX.XXX
+  struct sockaddr_in *remote;
+  int tmpres;
+  char *get;
+  char buf[BUFSIZ + 1];
 
-  SDL_Init(SDL_INIT_EVERYTHING);
-  if (SDLNet_Init() < 0) {
-  std:
-    std::cerr << "Failed to init net:  " << SDLNet_GetError() << std::endl;
-  }
-
-  const char* host = "date.jsontest.com";
-  IPaddress ip;
-  std::cout << "Resolving host:  " << host << std::endl;
-  SDLNet_ResolveHost(&ip, host, 80);
-  fprintf(stdout, "Resolved host to %d.%d.%d.%d port %hu\n",
-          ip.host >> 24, (ip.host >> 16) & 0xff, (ip.host >> 8) & 0xff,
-          ip.host & 0xff, ip.port);
+  fprintf(stdout, "Socket client example\n");
   fflush(stdout);
 
-  TCPsocket sock = SDLNet_TCP_Open(&ip);
-  // if (!sock) {
-    std::cerr << "Failed to connect socket:  " << SDLNet_GetError()
-              << std::endl;
-  // } else {
-    std::cout << "Opened socket:  " << sizeof sock << std::endl;
-  // }
+  // create tcp socket
+  WSADATA wsaData;
+  WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-  char* http;
-  sprintf(http, "GET / HTTP/1.1\nHost:  %s\n\n", host);
-  std::cout << "Command:  " << http << std::endl;
-  SDLNet_TCP_Send(sock, http, strlen(http) + 1);
-
-  char text[10000];
-  while (SDLNet_TCP_Recv(sock, http, 10000)) {
-    std::cout << "Response:  [" << text << "]" << std::endl;
+  client_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (client_socket < 0) {
+    fprintf(stdout, "client_socket = %d\n", client_socket);
+    fflush(stdout);
+    fprintf(stderr, "Can't create TCP socket\n");
+    fflush(stderr);
+    exit(1);
   }
 
-  SDLNet_TCP_Close(sock);
-  SDLNet_Quit();
-  SDL_Quit();
+  // get host ip
+  host = HOSTNAME;
+  fprintf(stdout, "Host:  %s\n", host);
+  fflush(stdout);
+  char *ip = (char *)malloc(iplen + 1);
+  memset(ip, 0, iplen + 1);
+  if ((hent = gethostbyname(host)) == NULL) {
+    fprintf(stderr, "Can't get IP");
+    fflush(stderr);
+    exit(1);
+  }
+  ip = inet_ntoa(*(struct in_addr *)*hent->h_addr_list);
+  fprintf(stdout, "The IP: %s\n", ip);
+  fflush(stdout);
+
+  // setup remote socket
+  remote = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in *));
+  remote->sin_family = AF_INET;
+  fprintf(stdout, "s_addr:%d\n", remote->sin_addr.s_addr);
+  fflush(stdout);
+  remote->sin_addr.s_addr = inet_addr(ip);
+  remote->sin_port = htons(PORT);
+  fprintf(stdout, "s_addr:%d\n", remote->sin_addr.s_addr);
+  fflush(stdout);
+
+  // have to read
+  // http://msdn.microsoft.com/en-us/library/windows/desktop/ms737625(v=vs.85).aspx
+
+  // connect socket
+  if (connect(client_socket, (struct sockaddr *)remote,
+              sizeof(struct sockaddr)) == SOCKET_ERROR) {
+    closesocket(client_socket);
+    fprintf(stderr, "Could not connect");
+    WSACleanup();
+    exit(1);
+  }
+
+  // prepare query
+  get = build_command_query(host, "/");
+  fprintf(stdout, "query: \n%s\n", get);
+  fflush(stdout);
+
+  // Send the query to the server
+  int sent = 0;
+  while (sent < strlen(get)) {
+    tmpres = send(client_socket, get + sent, strlen(get) - sent, 0);
+    if (tmpres == -1) {
+      fprintf(stderr, "Can't send query");
+      fflush(stderr);
+      exit(1);
+    }
+    sent += tmpres;
+  }
+
+  // now it is time to receive the page
+  memset(buf, 0, sizeof(buf));
+  int htmlstart = 0;
+  char *htmlcontent;
+  while ((tmpres = recv(client_socket, buf, BUFSIZ, 0)) > 0) {
+    if (htmlstart == 0) {
+      /* Under certain conditions this will not work.
+       * If the \r\n\r\n part is splitted into two messages
+       * it will fail to detect the beginning of HTML content
+       */
+      htmlcontent = strstr(buf, "\r\n\r\n");
+      if (htmlcontent != NULL) {
+        htmlstart = 1;
+        htmlcontent += 4;
+      }
+    } else {
+      htmlcontent = buf;
+    }
+    if (htmlstart) {
+      fprintf(stdout, htmlcontent);
+      fflush(stdout);
+    }
+
+    memset(buf, 0, tmpres);
+  }
+  if (tmpres < 0) {
+    fprintf(stderr, "Error receiving data");
+    fflush(stderr);
+  }
+
+  free(get);
+  free(remote);
+  closesocket(client_socket);
+  WSACleanup();
+
+  fprintf(stdout, "Program end");
+  fflush(stdout);
 
   return 0;
+}
+
+char *build_command_query(char *host, char *page) {
+  char *query;
+  char *getpage = page;
+  char *tpl = "GET /%s HTTP/1.0\nHost:  %s\nUser-Agent:  %s\n\n";
+  if (getpage[0] == '/') {
+    getpage = getpage + 1;
+    fprintf(stderr, "Removing leading \"/\", converting %s to %s\n", page,
+            getpage);
+    fflush(stderr);
+  }
+  query = (char *)malloc(strlen(host) + strlen(getpage) + strlen(USER_AGENT) +
+                         strlen(tpl) - 5);
+  sprintf(query, tpl, getpage, host, USER_AGENT);
+  return query;
 }
